af15b2096fbce31f8e0c6ee58d8f891d69d202e7
Kian Hunziker <kian.hunziker@unibas.ch>
2024-04-03 15:29:22 +0200

    add base class KitchenNet to compute data range and standardize forward call

--- models.py
+++ models.py
@@ -12,7 +12,33 @@ import matplotlib.pyplot as plt
 from tqdm import tqdm
 
 
-class SimpleMLP(Module):
+class KitchenNet(Module):
+    def __init__(self, num_kitchens: int, num_items: int, data_max: torch.Tensor, data_min: torch.Tensor):
+        super(KitchenNet, self).__init__()
+
+        self.data_max = data_max
+        self.data_min = data_min
+        self.data_range = data_max - data_min
+        self.data_range[self.data_range == 0] = 1.0
+
+        self.num_kitchens = num_kitchens
+        self.num_items = num_items
+
+        self.net = []
+
+    def forward(self, x: torch.Tensor, center_data: bool = False) -> torch.Tensor:
+        current_batch_size = x.shape[0]
+        if center_data:
+            # x = x - self.data_means
+            assert self.data_max is not None
+            assert self.data_min is not None
+
+            x = (x - self.data_min) / self.data_range
+            x = (x - 0.5) * 2.0
+        return self.net(x.reshape(current_batch_size, -1))
+
+
+class SimpleMLP(KitchenNet):
     def __init__(self, num_kitchens: int, num_items: int, hidden_layers: int = 3, hidden_features: int = 64,
                  data_means: float = 0.0, n_components: int = 0,
                  data_max: torch.Tensor = None, data_min: torch.Tensor = None):
@@ -26,21 +52,13 @@ class SimpleMLP(Module):
         :param data_max: Maxima of data, obtained from training data and saved for inference
         :param data_min: Minima of data, obtained from training data and saved for inference
         """
-        super(SimpleMLP, self).__init__()
-
-        self.data_means = data_means
-        self.data_max = data_max
-        self.data_min = data_min
-        self.data_range = data_max - data_min
-        self.data_range[self.data_range == 0] = 1.0
+        super().__init__(num_kitchens, num_items, data_max, data_min)
 
         # input consists of inventory for each kitchen plus food item to distribute
         if n_components == 0:
             input_dim = (num_kitchens + 1) * num_items
         else:
             input_dim = n_components
-
-        self.net = []
 
         # input layer
         self.net.append(nn.Linear(input_dim, hidden_features))
@@ -59,21 +77,78 @@ class SimpleMLP(Module):
 
         self.net = nn.Sequential(*self.net)
 
-    def forward(self, x: torch.Tensor, center_data: bool = False) -> torch.Tensor:
-        """
-        Forward pass. Input is reshaped to [batch_size, (num_kitchens + 1) * num_items]
-        :param center_data: If true, data mean will be subtracted before data is passed to network
-        :param x: Input with kitchen inventories and food item to distribute
-                    dimensions: [batch_size, (num_kitchens + 1), num_items]
-        :return: Softmax assignment to kitchens [batch_size, num_kitchens]
-        """
+
+class MLPVariableLayers(KitchenNet):
+    def __init__(self, num_kitchens: int, num_items: int, hidden_features: list = [64],
+                 data_max: torch.Tensor = None, data_min: torch.Tensor = None):
+        super().__init__(num_kitchens, num_items, data_max, data_min)
+
+        self.input_dim = (num_kitchens + 1) * num_items
+        temp_input_dim = self.input_dim
+
+        for i, hidden_dim in enumerate(hidden_features):
+            self.net.append(nn.Linear(temp_input_dim, hidden_dim))
+            self.net.append(nn.BatchNorm1d(hidden_dim))
+            # self.net.append(nn.Dropout(p=0.1))
+            self.net.append(nn.ReLU())
+            temp_input_dim = hidden_dim
+
+        # final output layer
+        self.net.append(nn.Linear(hidden_dim, num_kitchens))
+        self.net.append(nn.Softmax(dim=1))
+
+        self.net = nn.Sequential(*self.net)
+
+
+class CustomBatchNorm(nn.Module):
+    def __init__(self, num_inputs: int, hidden_features: int):
+        super(CustomBatchNorm, self).__init__()
+        self.hidden_features = hidden_features
+        self.linear = nn.Linear(num_inputs, hidden_features * 2)
+        self.norm = nn.BatchNorm1d(hidden_features, affine=False)
+
+    def forward(self, x: torch.Tensor, x_input: torch.Tensor) -> torch.Tensor:
+        alpha_beta = self.linear(x_input)
+        out = self.norm(x)
+        # out = torch.repeat_interleave(alpha_beta[:, 0].unsqueeze(1), self.hidden_features, dim=1) * out
+        # out = torch.repeat_interleave(alpha_beta[:, 1].unsqueeze(1), self.hidden_features, dim=1) + out
+        out = alpha_beta[:, :self.hidden_features] * out + alpha_beta[:, self.hidden_features:]
+        return out
+
+
+class ExperimentalModel(nn.Module):
+    def __init__(self, num_kitchens: int, num_items: int, hidden_layers: int = 3, hidden_features: int = 64,
+                 data_means: float = 0.0):
+        super(ExperimentalModel, self).__init__()
+        self.data_means = data_means
+        input_dim = (num_kitchens + 1) * num_items
+        self.extra_norm = nn.BatchNorm1d(input_dim)
+        self.first_layer = nn.Linear(input_dim, hidden_features)
+        self.norm_1 = CustomBatchNorm(input_dim, hidden_features)
+        self.relu_1 = nn.ReLU()
+
+        self.hidden_layer_1 = nn.Linear(hidden_features, hidden_features)
+        self.norm_2 = CustomBatchNorm(input_dim, hidden_features)
+        self.relu_2 = nn.ReLU()
+
+        # self.hidden_layer_2 = nn.Linear(hidden_features, hidden_features)
+        # self.norm_3 = CustomBatchNorm(input_dim, hidden_features)
+        # self.relu_3 = nn.ReLU()
+
+        self.final_layer = nn.Linear(hidden_features, num_kitchens)
+        self.softmax = nn.Softmax(dim=1)
+
+    def forward(self, x, center_data: bool = False) -> torch.Tensor:
         current_batch_size = x.shape[0]
         if center_data:
-            #x = x - self.data_means
-            assert self.data_max is not None
-            assert self.data_min is not None
+            x = x - self.data_means
+        x = x.reshape(current_batch_size, -1)
+        x = self.extra_norm(x)
+        out = self.first_layer(x)
+        out = self.norm_1(out, x)
+        out = self.relu_1(out)
+        out = self.relu_2(self.norm_2(self.hidden_layer_1(out), x))
+        # out = self.relu_3(self.norm_3(self.hidden_layer_2(out), x))
+        out = self.softmax(self.final_layer(out))
 
-            x = (x - self.data_min) / self.data_range
-            x = (x - 0.5) * 2.0
-        return self.net(x.reshape(current_batch_size, -1))
-
+        return out
